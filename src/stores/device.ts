4a2208f3bc448634febd491c41f7fc7c358e3ede
maji <maji@himaji.xyz>
2025-08-17 14:32:36 +0700

    Remove unnecessary imports

--- src/stores/device.ts
+++ src/stores/device.ts
@@ -1,8 +1,4 @@
-import type { Adb, AdbSync } from '@yume-chan/adb'
 import { defineStore } from 'pinia'
-import { connectDevice, getAdb, requestDevice } from '../utils/device'
-import { AdbDaemonWebUsbDevice } from '@yume-chan/adb-daemon-webusb'
-import { useStore } from './store'
 import type { IChartFile } from '../types'
 
 export const useDeviceStore = defineStore('device', {
